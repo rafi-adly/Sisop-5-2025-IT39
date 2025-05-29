2e7e769e4981948040f476dc489523ba1d230349
rafi-adly <rafiadly259@gmail.com>
2025-05-29 20:28:47 +0700

    Update std_lib.c

--- src/std_lib.c
+++ src/std_lib.c
@@ -51,27 +51,78 @@ int mod(int a, int b)
 
 bool strcmp(char *str1, char *str2)
 {
-  //TODO: Implementasi perbandingan string
+    while (*str1 && (*str1 == *str2)) {
+        str1++;
+        str2++;
+    }
+    return *str1 == *str2;
 }
 
 void strcpy(char *dst, char *src)
 {
-  //TODO: Implementasi penyalinan string
+  while ((*dest++ = *src++));
 }
 
 void clear(byte *buf, unsigned int size)
 {
-  //TODO: Implementasi pembersihan buffer
+    unsigned char *p = (unsigned char *)mem;
+    for (int i = 0; i < len; i++) {
+        p[i] = 0;
+    }
 }
 
 void atoi(char *str, int *num)
 {
-  //TODO: Implementasi konversi string ke integer
-  //NOTE: HARUS BISA ANGKA NEGATIF
+    int result = 0;
+    int sign = 1;
+
+    while (*str == ' ') str++;
+
+    if (*str == '-') {
+        sign = -1;
+        str++;
+    } else if (*str == '+') {
+        str++;
+    }
+
+    while (*str >= '0' && *str <= '9') {
+        result = result * 10 + (*str - '0');
+        str++;
+    }
+
+    *num = result * sign;
 }
 
 void itoa(int num, char *str)
 {
-  //TODO: Implementasi konversi integer ke string
-  //NOTE: HARUS BISA ANGKA NEGATIF
+    int i = 0;
+    int is_negative = 0;
+
+    if (num == 0) {
+        str[i++] = '0';
+        str[i] = '\0';
+        return;
+    }
+
+    if (num < 0) {
+        is_negative = 1;
+        num = -num;
+    }
+
+    while (num > 0) {
+        str[i++] = (num % 10) + '0';
+        num /= 10;
+    }
+
+    if (is_negative) {
+        str[i++] = '-';
+    }
+
+    str[i] = '\0';
+
+    for (int j = 0, k = i - 1; j < k; j++, k--) {
+        char temp = str[j];
+        str[j] = str[k];
+        str[k] = temp;
+    }
 }
