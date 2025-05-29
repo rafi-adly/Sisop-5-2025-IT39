29fa67a0cbb9c6d050036b126a5146f08bc930c4
rafi-adly <rafiadly259@gmail.com>
2025-05-29 20:10:58 +0700

    Update std_lib.c

--- src/std_lib.c
+++ src/std_lib.c
@@ -2,13 +2,51 @@
 
 int div(int a, int b)
 {
-  //TODO: Implementasi pembagian
-  //NOTE: HARUS BISA ANGKA NEGATIF
+    if (b == 0) return 0; 
+    int negatif = 0;
+    if (a < 0) { a = -a; negatif ^= 1; }
+    if (b < 0) { b = -b; negatif ^= 1; }
+
+    int hasil = 0;
+    int cek_pembagi = b;
+    int shift = 0;
+    while ((cek_pembagi << 1) <= a) {
+        cek_pembagi <<= 1;
+        shift++;
+    }
+
+    while (shift >= 0) {
+        if (a >= cek_pembagi) {
+            a -= cek_pembagi;
+            hasil |= (1 << shift);
+        }
+        cek_pembagi >>= 1;
+        shift--;
+    }
+
+    return negatif ? -hasil : hasil;
 }
 
 int mod(int a, int b)
 {
-  //TODO: Implementasi modulus
+  if (b == 0) return -1;
+    int cek_pembagi = b;
+    int shift = 0;
+
+    while ((temp_b << 1) <= a) {
+        cek_pembagi <<= 1;
+        shift++;
+    }
+
+    while (shift >= 0) {
+        if (a >= cek_pembagi) {
+            a -= cek_pembagi;
+        }
+        cek_pembagi >>= 1;
+        shift--;
+    }
+
+    return a;
 }
 
 bool strcmp(char *str1, char *str2)
